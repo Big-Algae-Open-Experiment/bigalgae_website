fc925598d6f46fae2d700377601876d8f6b0af1a
Marc Jones <marc.jones@jic.ac.uk>
2015-11-30 17:13:19 +0000

    Made the database backup a Cron job which is set up by Ansible
    The MongoDB database backup is now executed by Cron every 12 hours
    at 00:00 and 12:00. In order to save space on the server, only the
    last 5 backups are kept. This number can be changed in the
    mongodump_backup.sh script.

--- server_configuration_files/mongodump_backup.sh
+++ server_configuration_files/mongodump_backup.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-BACKUP_FOLDER="/var/www/html/baoe-app/backups/$(date +baoe_backup_%Y_%m_%d_%H_%M)"
+BACKUP_FOLDER="/var/www/html/baoe-app/backups/"
 
-mkdir -p $BACKUP_FOLDER/images
-mkdir -p $BACKUP_FOLDER/mongodb
+# Create a new backup
+CURRENT_FOLDER=$BACKUP_FOLDER$(date -u +baoe_backup_%Y_%m_%d_%H_%M)
 
-cp /var/www/html/baoe-app/images/* $BACKUP_FOLDER/images/
-mongodump -o $BACKUP_FOLDER/mongodb
+mkdir -p $CURRENT_FOLDER/images
+mkdir -p $CURRENT_FOLDER/mongodb
+
+cp /var/www/html/baoe-app/images/* $CURRENT_FOLDER/images/
+mongodump -o $CURRENT_FOLDER/mongodb
+
+# Delete old backups
+BACKUP_FOLDERS=($(ls $BACKUP_FOLDER))
+
+NUMBER_OF_BACKUPS_TO_KEEP=5
+
+for ((i=0; i<${#BACKUP_FOLDERS[@]}-$NUMBER_OF_BACKUPS_TO_KEEP; i++));
+do
+    rm -rf $BACKUP_FOLDER${BACKUP_FOLDERS[$i]}
+done
