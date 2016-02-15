c12828fcef7dad3ebde82a484a4b8435b1046055
Marc Jones <marc.jones@jic.ac.uk>
2016-02-15 13:44:38 +0000

    Fixed bug in database restore script
    The default behaviour of mongorestore is to not overwrite an entry in the database. This meant that some fields were not being properly restored to the database. The fix involves dropping the bigalgae database before restoring it.

--- server_configuration_files/mongorestore.sh
+++ server_configuration_files/mongorestore.sh
@@ -7,4 +7,5 @@ BACKUPS=($(ls $BACKUP_FOLDER ))
 MOST_RECENT_BACKUP=${BACKUPS[${#BACKUPS[@]} - 1]}
 
 cp $BACKUP_FOLDER/$MOST_RECENT_BACKUP/images/* /var/www/html/baoe-app/images/
+mongo bigalgae --eval "db.dropDatabase()"
 mongorestore $BACKUP_FOLDER/$MOST_RECENT_BACKUP/mongodb
